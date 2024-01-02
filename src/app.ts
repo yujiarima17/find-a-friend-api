369ad27ea41edd436778b10b9d6001e721b5e85a
Yuji Arima <arimayuji2@gmail.com>
2024-01-02 19:48:45 -0300

    feat: error handler plugin

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
 import fastify from "fastify";
+import { orgsRoutes } from "./http/controllers/orgs/routes";
+import { ZodError } from "zod";
+import { env } from "./env";
 
 export const app = fastify();
+
+app.register(orgsRoutes);
+
+app.setErrorHandler((error, request, reply) => {
+	if (error instanceof ZodError) {
+		return reply
+			.status(400)
+			.send({ message: "Validation Error", issues: error.format() });
+	}
+	
+	if (env.NODE_ENV !== "production") {
+		console.error(error);
+	}
+
+	return reply.status(500).send({ message: "Internal Server Error!" });
+});
