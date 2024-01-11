83d90f39e26514c5fe2b2299005f4f368744e5ce
Yuji Arima <arimayuji2@gmail.com>
2024-01-11 18:26:49 -0300

    refresh token route

--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -2,8 +2,11 @@ import { FastifyInstance } from "fastify";
 import { register } from "./register";
 import { authenticate } from "./authenticate";
 import { details } from "./details";
+import { verifyJWT } from "@/http/middlewares/verfiy-jwt";
+import { refresh } from "./refresh";
 export async function orgsRoutes(app: FastifyInstance) {
 	app.post("/orgs", register);
-	app.get("/details",details)
+	app.get("/details", { onRequest: [verifyJWT] }, details);
 	app.post("/sessions", authenticate);
+	app.patch("/token/refresh", refresh);
 }
