ed394bd4fc20839021f7c8001e642fdfde5c0d03
Yuji Arima <arimayuji2@gmail.com>
2024-01-11 19:35:53 -0300

    cleaning console logs

--- src/http/middlewares/verfiy-jwt.ts
+++ src/http/middlewares/verfiy-jwt.ts
@@ -3,9 +3,7 @@ import { FastifyReply, FastifyRequest } from "fastify";
 export async function verifyJWT(request: FastifyRequest, reply: FastifyReply) {
 	
 	try {
-		console.log(request);
 		await request.jwtVerify();
-		console.log(request.user);
 	} catch (error) {
 		return reply.status(401).send({ message: "Unauthorized" });
 	}
