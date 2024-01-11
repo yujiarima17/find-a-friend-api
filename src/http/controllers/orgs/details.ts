1b71c09d0bdaed2f57c4d65b647ea414a924a8df
Yuji Arima <arimayuji2@gmail.com>
2024-01-11 18:27:19 -0300

    feat: RBAC verify

--- src/http/controllers/orgs/details.ts
+++ src/http/controllers/orgs/details.ts
@@ -1,7 +1,18 @@
+import { MakeGetOrgDetailsUseCase } from "@/use-cases/factories/orgs/make-get-org-details";
 import { FastifyReply, FastifyRequest } from "fastify";
 
 export async function details(request: FastifyRequest, reply: FastifyReply) {
-	await request.jwtVerify();
-    
-	return reply.status(200).send();
+
+	const getOrgDetails = MakeGetOrgDetailsUseCase();
+
+	const { org } = await getOrgDetails.execute({
+		id: request.user.sub,
+	});
+
+	return reply.status(200).send({
+		org: {
+			...org,
+			password_hash: undefined,
+		},
+	});
 }
